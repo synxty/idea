c21deeaa04a34a7e04b0e33d7fffe764466442bd
Pedro Resende <ceo.synxty@gmail.com>
2023-11-25 23:42:11 +0000

    fix game title

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: '_game-title_ Docs',
-  description: 'Read everything about _game-title_ here!',
+  title: 'Synxty Docs',
+  description: 'Read everything about Synxty here!',
   base: '/idea/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
